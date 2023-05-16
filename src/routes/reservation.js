03c73a54ff5a7469c6499baa1e059d23ec657b78
unknown <victor.llancari@unmsm.edu.pe>
2023-05-16 14:03:35 -0500

    fix

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -3,7 +3,7 @@ const reservationController = require('../controller/reservation.controller');
 
 const router=express.Router();
 
-//crear restaurante
+//crear reservacion
 router.post('/reservation', (req,res)=>{
     reservationController.createReservation(req.body)
     .then(reservation=>{
@@ -14,64 +14,12 @@ router.post('/reservation', (req,res)=>{
     })
 });
 
-//obtener todos los restaurantes
+//obtener todos los reservaciones
 router.get('/reservations', (req,res)=>{
     reservationController.getAllReservation()
     .then(reservations => res.json(reservations))
     .catch(err => {
         res.status(500).send("error al  leer los reservaciones de la base de datos")
-
-    })
-});
-
-//obtener un restaurant
-router.get('/restaurant/:id', (req,res)=>{
-    let id = req.params.id
-    restaurantController.getRestaurant(id)
-    .then(restaurant=>{
-        if(!restaurant){
-            res.status(404).send(`Restaurante con id [${id}] no existe.`)
-        }else{
-            res.json(restaurant)
-        }
-    })
-    .catch(err=>{
-        res.status(500).send(`Error ocurrio obteniendo restaurante con id [${id}]`)
-    })
-});
-
-//actualizar un restaurant
-router.put('/restaurant/:id', (req,res)=>{
-    let id = req.params.id
-    let requestRestaurant = req.body
-    restaurantController.updateRestaurant(id, requestRestaurant)
-    .then(restaurant=>{
-        res.json(restaurant)
-    })
-    .catch(err =>{
-        res.status(500).send(`Error ocurrio actualizando restaurante con id [${id}]`)
-    })
-});
-
-//eliminar un restaurant
-router.delete('/restaurant/:id', (req,res)=>{
-    let id = req.params.id
-    restaurantController.deleteRestaurant(id)
-    .then(restaurant=>{
-        res.json(restaurant)
-    })
-    .catch(err =>{
-        res.status(500).send(`Error ocurrio eliminando restaurante con id [${id}]`)
-    })
-});
-
-//obtener todos los restaurantes
-router.get('/restaurants/:search', (req,res)=>{
-    let search = req.params.search
-    restaurantController.SearchRestaurant(search)
-    .then(restaurants => res.json(restaurants))
-    .catch(err => {
-        res.status(500).send("error al buscar los restaurantes de la base de datos")
 
     })
 });
